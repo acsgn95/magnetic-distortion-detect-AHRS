d3718e9e9ec2ee21b3ebcbc27291eec7ac200957
Ahmet Coşgun <78763530+acsgn95@users.noreply.github.com>
2022-05-17 01:51:19 +0300

    Add files via upload

--- quaternion.py
+++ quaternion.py
@@ -2,11 +2,7 @@ import numpy as np
 from math import *
 
 def euler2quaternion(roll,pitch,yaw):
-    """
-        roll: radians
-        pitch: radians
-        yaw: radians
-    """
+
     r = roll/2
     p = pitch/2
     y = yaw/2
@@ -25,22 +21,34 @@ def euler2quaternion(roll,pitch,yaw):
 
 
 def quaternion2euler(qw,qx,qy,qz):
+
     roll = atan2((2*(qw*qx+qy*qz)),(1-2*qx*qx-2*qy*qy))
-    pitch = asin(2*(qw*qy-qx-qz))
-    yaw = atan2((2*(qw*qz+qx*qy)),(1-2*qy*qy-qz*qz))
+    pitch = asin(2*(qw*qy-qx*qz))
+    yaw = atan2((2*(qw*qz+qx*qy)),(1-2*qy*qy-2*qz*qz))
 
     return roll,pitch,yaw
 
 
 def CTMquaternion(qw,qx,qy,qz):
 
-    C = np.array([[(qw*qw+qx*qx-qy*qy-qz*qz),(2*(qx*qy+qw*qz)),(2*(qx*qz-qy*qw))],
+    Cnb = np.array([[(qw*qw+qx*qx-qy*qy-qz*qz),(2*(qx*qy+qw*qz)),(2*(qx*qz-qy*qw))],
                   [(2*(qx*qy-qw*qz)),(qw*qw-qx*qx+qy*qy-qz*qz),(2*(qy*qz+qw*qx))],
-                  [(2*(qx*qz+qy*qw)),(2*(qy*qz+qw*qx)),(qw*qw-qx*qx-qy*qy+qz*qz)]])
+                  [(2*(qx*qz+qy*qw)),(2*(qy*qz-qw*qx)),(qw*qw-qx*qx-qy*qy+qz*qz)]])
 
-    Ct = C.transpose()
+    Cbn = np.transpose(Cnb)
 
-    return C
+    return Cbn,Cnb
 
 
-    
+def qnorm(qw,qx,qy,qz):
+
+    norm = (qw*qw+qx*qx+qy*qy+qz*qz)**0.5
+
+    qwn = qw/norm
+    qxn = qx/norm
+    qyn = qy/norm
+    qzn = qz/norm
+
+    return qwn,qxn,qyn,qzn
+
+
